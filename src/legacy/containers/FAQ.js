2e13ac98f30d47d0e4fc458da41958cf4aeda5c9
greatPurpose <happy.goodday@outlook.com>
2016-09-29 16:39:35 +0900

    FAQ.js added

---
+++ src/legacy/containers/FAQ.js
@@ -0,0 +1,22 @@
+// © 2017 VARRO ANALYTICS. ALL RIGHTS RESERVED.
+import React, { Component } from 'react';
+import TopNavbar from '../components/TopNavbar';
+
+import './FAQ.css';
+
+export default class FAQ extends Component {
+  render() {
+    return (
+      <div className="FAQ">
+        <TopNavbar user={this.props.user}/>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              fasdfdasf sd sdaf
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
