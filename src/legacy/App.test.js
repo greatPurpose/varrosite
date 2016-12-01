5e5df19ec0ca3103825349921f1c26be79724073
greatPurpose <happy.goodday@outlook.com>
2016-12-01 11:30:53 +0900

    App.test.js added

---
+++ src/legacy/App.test.js
@@ -0,0 +1,9 @@
+// © 2017 VARRO ANALYTICS. ALL RIGHTS RESERVED.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
