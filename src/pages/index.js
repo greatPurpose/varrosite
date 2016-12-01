1623622439d39af01ec5ccfcfe3b6f9691fd0ce2
greatPurpose <happy.goodday@outlook.com>
2016-12-01 18:39:43 +0900

    index.js added

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/grid/Layout'
+
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Index page</h1>
+  </Layout>
+)
+
+export default IndexPage;
