42e6a239b2d4c898ddfeddc5f240edfb8c08955e
greatPurpose <happy.goodday@outlook.com>
2017-06-23 14:13:37 +0900

    404.js added

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../components/grid/Layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div id="wrapper">
+      <section>
+        <div className="container">
+          <section>
+            <h1>NOT FOUND</h1>
+            <p>Not a valid URL</p>
+          </section>
+        </div>
+      </section>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
