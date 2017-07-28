9c6d2764d15552b590ea4d009d283b6050c71ac1
greatPurpose <happy.goodday@outlook.com>
2017-07-28 12:05:13 +0900

    CompanyProfile.js added

---
+++ src/legacy/components/fundraising/CompanyProfile.js
@@ -0,0 +1,14 @@
+// © 2017 VARRO ANALYTICS. ALL RIGHTS RESERVED.
+import React, { Component } from 'React';
+
+
+export default class CompanyProfile extends Component {
+
+  render() {
+    return (
+      <div className="CompanyProfile">
+
+      </div>
+    )
+  }
+}
